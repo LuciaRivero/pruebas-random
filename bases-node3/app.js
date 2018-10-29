2824a9fe34d6025c96ba0014a7b8061d204c4843
Lucia <claudialucia.rivero@gmail.com>
2018-10-29 17:56:24 -0300

    hasta aca llegue xD

--- bases-node3/app.js
+++ bases-node3/app.js
@@ -9,30 +9,28 @@ const argv = require('yargs')
             default: 10
         }
     })
+    .help()
     .argv;
 
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
+let argv2 = process.argv;
+console.log('Limite', argv.limite);
 
 
 
 //console.log(process.argv);
-let argv2 = process.argv;
-
-console.log('Limite', argv.limite);
 //console.log(argv.base);
-
 //let parametro = argv[2];
 //let base = parametro.split('=')[1]
 //split convetir de un string a un arreglo
-//console.log(base);
 /*
+//console.log(base);
 [ 'C:\\Program Files\\nodejs\\node.exe',
   'C:\\Users\\lucia.rivero\\Desktop\\cursoUdemy\\Node-Fer-Herrera\\bases-node3\\app.js' ]
 */
-
 /*
 crearArchivo(base)
     .then(archivo => console.log(`Archivo creado: ${archivo}`))
     .catch(e => console.log(e));
-    */
+*/

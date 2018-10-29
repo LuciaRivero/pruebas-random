a57a5cd117d93bd7a98b84c4b0ff0d43f26c3d04
Lucia <claudialucia.rivero@gmail.com>
2018-10-29 11:26:47 -0300

    sumamos weas

--- fundamentos/promesas.js
+++ fundamentos/promesas.js
@@ -1,3 +1,4 @@
+//DB de empleados
 let empleados = [{
     id: 1,
     nombre: 'Fer'
@@ -9,6 +10,7 @@ let empleados = [{
     nombre: 'owen'
 }];
 
+//DB de salarios.
 let salarios = [{
     id: 1,
     salario: 1000
@@ -17,19 +19,41 @@ let salarios = [{
     salario: 2000
 }]
 
+/* Promesa es una funcion con dos callbacks (resolve reject) resolve se llama si la promesa es exitosa, y el reject si no es exitosa (en este caso no existe empleado en la db). En el resolve no podemos mandar dos arguentos o mas solo se puede regresar uno, si necesitamos mandar mas de un dato debemos mandar un objeto*/
+let getEmpleado = (id) => { //recibo un unico parametro id
+        return new Promise((resolve, reject) => {
+            let empleadoDB = empleados.find(empleado => empleado.id === id)
+            if (!empleadoDB) {
+                reject(`No existe un empleado con el ID ${id}`)
+            } else {
+                resolve(empleadoDB);
+            }
+        });
 
-let getEmpleado = (id) => {
+    }
+    //si llamamos dos o mas veces el resolve se dispara una sola vez
+let getSalario = (empleado) => {
     return new Promise((resolve, reject) => {
-        let empleadoDB = empleados.find(empleado => empleado.id === id)
-        if (!empleadoDB) {
-            reject(`No existe un empleado con el ID ${id}`)
+        let salarioDB = salarios.find(salario => salario.id === empleado.id);
+        if (!salarioDB) {
+            reject(`No existe salario para el empleado ${empleado.nombre}`)
         } else {
-            resolve(empleadoDB);
+            resolve({
+                nombre: empleado.nombre,
+                salario: salarioDB.salario,
+                id: empleado.id
+            });
         }
     });
-
 }
 
-getEmpleado(12).then(empleado => {
-    console.log('Empleado de BD', empleado);
-})
+
+getEmpleado(3).then(empleado => {
+    getSalario(empleado).then(resp => {
+        console.log(`el salario de ${resp.nombre} es de ${resp.salario}$ `);
+    }, (err) => {
+        console.log(err);
+    })
+}, (err) => {
+    console.log(err);
+});

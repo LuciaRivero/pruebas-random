bc5321da4dec9f53cecb9b4d2a22706f337a3f91
Lucia <claudialucia.rivero@gmail.com>
2018-10-29 17:41:13 -0300

    que carajada es esto :(

--- bases-node3/app.js
+++ bases-node3/app.js
@@ -1,18 +1,38 @@
+const argv = require('yargs')
+    .command('listar', 'imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .argv;
+
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
 
+
+
 //console.log(process.argv);
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1]
-    //split convetir de un string a un arreglo
-    //console.log(base);
-    /*
-    [ 'C:\\Program Files\\nodejs\\node.exe',
-      'C:\\Users\\lucia.rivero\\Desktop\\cursoUdemy\\Node-Fer-Herrera\\bases-node3\\app.js' ]
-    */
+let argv2 = process.argv;
 
+console.log('Limite', argv.limite);
+//console.log(argv.base);
 
+//let parametro = argv[2];
+//let base = parametro.split('=')[1]
+//split convetir de un string a un arreglo
+//console.log(base);
+/*
+[ 'C:\\Program Files\\nodejs\\node.exe',
+  'C:\\Users\\lucia.rivero\\Desktop\\cursoUdemy\\Node-Fer-Herrera\\bases-node3\\app.js' ]
+*/
+
+/*
 crearArchivo(base)
     .then(archivo => console.log(`Archivo creado: ${archivo}`))
     .catch(e => console.log(e));
+    */
